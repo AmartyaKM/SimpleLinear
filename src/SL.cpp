82ed39563b421d017bf0e4ad87c0fbb91d77caf2
AmartyaKM <amartyamaulik1998@gmail.com>
2024-01-29 14:55:36 -0700

    Simple Linear regression using Rcpp

---
+++ src/SL.cpp
@@ -0,0 +1,42 @@
+#include <RcppArmadillo.h>
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
+using namespace Rcpp;
+using namespace arma;
+
+// [[Rcpp::export]]
+List SimpLinCpp(NumericVector x, NumericVector y) {
+  
+  mat data = join_rows(ones<vec>(x.size()), vec(x));
+  
+  vec response = vec(y.begin(), y.size(), false);
+ 
+  vec coefficients = solve(data, response);
+  vec residuals = response - data * coefficients;
+  vec fitted_values = data * coefficients;
+  
+  double s2 = sum(square(residuals)) / (x.size() - 2);  // Residual standard error
+  mat cov_matrix = s2 * inv_sympd(trans(data) * data);
+  vec se = sqrt(cov_matrix.diag());
+  double t_critical = R::qt(0.975, x.size() - 2, true, false); // Use R::qt for quantile function
+  
+  vec lower_ci = coefficients - t_critical * se;
+  vec upper_ci = coefficients + t_critical * se;
+  
+  // Create a list with the results
+  List result = List::create(
+    _["coefficients"] = coefficients,
+    _["standard_errors"] = se,
+    _["confidence_intervals"] = DataFrame::create(
+      _["lower"] = lower_ci,
+      _["upper"] = upper_ci
+    ),
+    _["residuals"] = residuals,
+    _["fitted_values"] = fitted_values
+  );
+  
+  return result;
+}
+
+
